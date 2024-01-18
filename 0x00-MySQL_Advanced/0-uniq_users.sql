4994440e28cdb667e913322042e6eb5d0c2fe994
azeadrr <azeadn1@gmail.com>
2024-01-18 12:27:17 -0800

    mandatory tasks

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,5 +1,5 @@
---We are all unique!
---SQL script that creates a table users
+-- 0. We are all unique!
+-- creates a table `users` with: id, email and name
 
 CREATE TABLE IF NOT EXISTS `users` (
     id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
