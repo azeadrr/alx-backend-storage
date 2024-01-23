2fb13655d33e06c40ab1cd8302575e62d9268ebe
azeadrr <azeadn1@gmail.com>
2024-01-23 12:27:42 -0800

    advanced tasks

---
+++ 0x01-NoSQL/102-log_stats.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+"""Log stats"""
+from pymongo import MongoClient
+
+
+def log_stats(mongo_collection, option=None):
+    """script that provides some stats
+    about Nginx logs stored in MongoDB
+    """
+    METHODS = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+    items = {}
+    if option:
+        vl = mongo_collection.count_documents(
+            {"method": {"$regex": option}})
+        print(f"\tmethod {option}: {vl}")
+        return
+
+    rslt = mongo_collection.count_documents(items)
+    print(f"{rslt} logs")
+    print("Methods:")
+    for method in METHODS:
+        log_stats(nginx_collection, method)
+    stts_check = mongo_collection.count_documents({"path": "/status"})
+    print(f"{stts_check} status check")
+
+
+if __name__ == "__main__":
+    nginx_collection = MongoClient('mongodb://127.0.0.1:27017').logs.nginx
+    log_stats(nginx_collection)
